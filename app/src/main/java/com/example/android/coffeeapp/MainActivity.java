67b427467a090d318bc79a545ec7eda76a1f01d9
Capri84 <capri84@mail.ru>
2018-02-04 18:19:29 +0300

    Refreshment

--- app/src/main/java/com/example/android/coffeeapp/MainActivity.java
+++ app/src/main/java/com/example/android/coffeeapp/MainActivity.java
@@ -1,13 +1,95 @@
+/**
+ * IMPORTANT: Add your package below. Package name can be found in the project's AndroidManifest.xml file.
+ * This is the package name our example uses:
+ *
+ * package com.example.android.justjava;
+ *
+ */
 package com.example.android.coffeeapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
+import com.example.android.coffeeapp.R;
+
+import java.text.NumberFormat;
+
+/**
+ * This app displays an order form to order coffee.
+ */
 public class MainActivity extends AppCompatActivity {
+
+    int quantity = 0;
+    int price = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public void submitOrder(View view) {
+        displayPrice (quantity * price);
+    }
+
+    /**
+     * This method is called when the Espresso button is clicked.
+     */
+    public void espresso(View view) {
+        price = 3;
+        display (quantity);
+    }
+
+    /**
+     * This method is called when the Americano button is clicked.
+     */
+    public void americano(View view) {
+        price = 5;
+        display (quantity);
+    }
+
+    /**
+     * This method is called when the Cappuccino button is clicked.
+     */
+    public void cappuccino(View view) {
+        price = 7;
+        display (quantity);
+    }
+
+    /**
+     * This method is called when the plus button is clicked.
+     */
+    public void increment(View view) {
+        quantity = quantity + 1;
+        display (quantity);
+    }
+
+    /**
+     * This method is called when the minus button is clicked.
+     */
+    public void decrement(View view) {
+        quantity = quantity - 1;
+        display (quantity);
+    }
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+
+    /**
+     * This method displays the given price on the screen.
+     */
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 }
